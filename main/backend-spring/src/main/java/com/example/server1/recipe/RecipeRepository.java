b2d127b15f7586dc73e8c2f3493557bd173e1e8e
Felix <agene001@umn.edu>
2025-03-25 09:01:37 -0500

    Merge remote-tracking branch 'workout-app/master'

--- main/backend-spring/src/main/java/com/example/server1/recipe/RecipeRepository.java
+++ main/backend-spring/src/main/java/com/example/server1/recipe/RecipeRepository.java
@@ -9,7 +9,8 @@ import java.util.Optional;
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     @Query(value="SELECT * FROM recipes s ORDER BY RAND() LIMIT ?1", nativeQuery = true)
     List<Recipe> findAllSample(int k);
-
+    @Query(value="SELECT * FROM recipes s",nativeQuery = true)
+    List<Recipe> findAll();
     @Query(value="SELECT * FROM recipes s WHERE s.name=?1 ORDER BY RAND()", nativeQuery = true)
     Recipe getRecipe(String query);
 
