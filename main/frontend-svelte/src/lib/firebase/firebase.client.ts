69c37156b09f8d3c03cdb1d318e3276269b8c240
Felix <agene001@umn.edu>
2025-05-03 16:23:58 -0500

    Docker, dockercompose and kubernetes setup

--- main/frontend-svelte/src/lib/firebase/firebase.client.ts
+++ main/frontend-svelte/src/lib/firebase/firebase.client.ts
@@ -7,12 +7,12 @@ const env = import.meta.env;
 
 const firebaseConfigClient = {
     apiKey: env.PUBLIC_FIREBASE_API_KEY,
-    authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: env.VITE_FIREBASE_APP_ID,
-    measurementId: env.VITE_FIREBASE_MEASUREMENT_ID
+    authDomain: env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: env.PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: env.PUBLIC_FIREBASE_APP_ID,
+    measurementId: env.PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Client-Side App (ensure singleton pattern)
