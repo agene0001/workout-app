b52326963accc8f158bc70d9d7b84205f444bf2d
Felix <agene001@umn.edu>
2025-05-03 16:20:36 -0500

    Docker, dockercompose and kubernetes setup

--- main/frontend-svelte/src/lib/firebase/firebase.client.ts
+++ main/frontend-svelte/src/lib/firebase/firebase.client.ts
@@ -3,10 +3,10 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 // Import PUBLIC variables - these MUST start with VITE_ and be set at BUILD time
-const env = import.meta.env.VITE_FIREBASE_API_KEY!=null ? import.meta.env : process.env;
+const env = import.meta.env;
 
 const firebaseConfigClient = {
-    apiKey: env.VITE_FIREBASE_API_KEY,
+    apiKey: env.PUBLIC_FIREBASE_API_KEY,
     authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
